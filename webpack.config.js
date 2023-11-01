7894783c240a04507a51b93fd1f2201df0311902
Julio Asec Ed <ed_julio.test@outlook.com>
2023-11-01 20:15:55 -0300

    configuração dos loadres sass e css

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,20 @@ module.exports = {
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, "dist")
-    }
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i, use: ['style-loader', 'css-loader'],
+          },
+
+          {
+            test: /\.s[ac]ss$/i, use: ['style-loader', 'css-loader', "sass-loader"]
+          }
+
+        ],
+      }
+
+
 }
 
